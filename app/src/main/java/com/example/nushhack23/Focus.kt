ac9474768113202f2488d26473174c78c278128f
neowz <46238360+nwztest@users.noreply.github.com>
2023-11-10 22:54:51 +0800

    idk at this point

--- app/src/main/java/com/example/nushhack23/Focus.kt
+++ app/src/main/java/com/example/nushhack23/Focus.kt
@@ -5,11 +5,19 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
+
+data class FocusGame(val name: String, val description: String, val imageVector: ImageVector)
 
 @Composable
 fun FocusFragment() {
     Text(text = "Focus test", modifier = Modifier.fillMaxSize(), color= Color.Red)
+}
+
+@Composable
+fun GameCard() {
+
 }
 
 @Preview
--- app/src/main/java/com/example/nushhack23/Relax.kt
+++ app/src/main/java/com/example/nushhack23/Relax.kt
@@ -1,16 +1,20 @@
 package com.example.nushhack23
 
 import android.content.Intent
-import android.graphics.drawable.Icon
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
@@ -26,19 +30,31 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.lazy.items
 
-
+data class ChatActivity(val name: String, val description: String, val id: Int)
+val chatActivities = listOf(
+    ChatActivity("Grounding", "Utilise the 54321 Technique", R.drawable.grounded),
+    ChatActivity("Jokes", "Have some fun, lighten up", R.drawable.laugh),
+    ChatActivity("Chat", "Just a normal chat session", R.drawable.chat)
+)
 
 @Composable
 fun RelaxFragment() {
-    Text(text = "Relax Test", modifier = Modifier.fillMaxSize(), color= Color.Green)
+    Column() {
+        DeepBreathingCard()
+        Spacer(modifier = Modifier.size(20.dp))
+        Text("Chatbot", fontSize = 28.sp, modifier=Modifier.padding(10.dp))
+        LazyRelax()
+    }
 }
 
 @Composable
 fun DeepBreathingCard() {
     Surface(shadowElevation = 20.dp,
         modifier = Modifier
-            .border(BorderStroke(2.dp, Color.Black), RoundedCornerShape(30.dp))
+            .padding(10.dp)
+            .border(BorderStroke(1.dp, Color.Black), RoundedCornerShape(30.dp))
             .clip(RoundedCornerShape(30.dp))
     ) {
         Box() {
@@ -58,10 +74,41 @@ fun DeepBreathingCard() {
 
 
 @Composable
-fun ChatCard(imageVector: ImageVector) {
-    Column() {
-        Icon(imageVector = imageVector, contentDescription = "Image of type of talking")
-        Text("")
+fun ChatCard(chatActivity: ChatActivity) {
+    Surface(color=Color.Yellow, modifier = Modifier
+        .fillMaxWidth()
+        .clip(RoundedCornerShape(10.dp))
+        .border(BorderStroke(1.dp, Color.Black), RoundedCornerShape(10.dp))
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp),
+            horizontalArrangement = Arrangement.spacedBy(10.dp)
+        ) {
+            Icon(
+                painter = painterResource(id = chatActivity.id),
+                contentDescription = "Image for ${chatActivity.name}",
+                modifier = Modifier
+                    .size(100.dp, 100.dp)
+                    .align(Alignment.CenterVertically)
+            )
+            Column(modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.CenterVertically)) {
+                Text(chatActivity.name, fontSize = 48.sp)
+                Text(chatActivity.description, fontSize = 24.sp, modifier = Modifier.fillMaxWidth())
+            }
+        }
+    }
+}
+
+@Composable
+fun LazyRelax() {
+    LazyRow(horizontalArrangement = Arrangement.spacedBy(4.dp), contentPadding = PaddingValues(10.dp, 10.dp)) {
+        items(chatActivities) {chatActivity ->
+            ChatCard(ChatActivity(chatActivity.name, chatActivity.description, chatActivity.id))
+        }
     }
 }
 
@@ -70,5 +117,5 @@ fun ChatCard(imageVector: ImageVector) {
 @Preview
 @Composable
 fun preview1() {
-    DeepBreathingCard()
+    LazyRelax()
 }
